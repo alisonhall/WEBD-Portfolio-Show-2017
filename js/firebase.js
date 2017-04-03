1980bc73fa17cbdecd3c6050a3b3bc5e4f6efc83
Chris Wilczewski <c.wilczewski@gmail.com>
2017-04-03 15:23:47 -0400

    added the whole link pagination stuffs in the good ol js file

--- js/firebase.js
+++ js/firebase.js
@@ -46,3 +46,38 @@ setTimeout(function () {
 $('section').horizon({
     swipe: false
 });
+
+
+var aChildren = $("nav li").children().not('.eventbrite-link'); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {    
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollLeft(); // get the offset of the window from the left of page
+        var windowWidth = $(window).width(); // get the width of the window
+        var docWidth = $(document).width();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().left; // get the offset of the div from the left of page
+            var divWidth = $(theID).width(); // get the width of the div in question
+            if (windowPos >= divPos && windowPos < (divPos + divWidth)) {
+                $("a[href='" + theID + "']").parent().addClass("selected");
+                console.log(theID);
+            } else {
+                $("a[href='" + theID + "']").parent().removeClass("selected");
+            }
+        }
+
+        if(windowPos + windowWidth == docWidth) {
+            if (!$("nav li:last-child a").hasClass("selected")) {
+                var navActiveCurrent = $(".selected").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("selected");
+                $("nav li:last-child a").addClass("selected");
+            }
+        }
+    });
